628c826016771a1296db77ae65a96b1b706e94ed
VTheSteveV <56842674+VTheSteveV@users.noreply.github.com>
2021-01-27 21:29:32 +0100

    Solution

---
+++ ContactList/ContactList/Controllers/ContactListController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using ContactList.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactList.Controllers {
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ContactListController : ControllerBase {
+        private readonly ContactRepository repository;
+
+        public ContactListController(ContactRepository repository) {
+            this.repository = repository;
+        }
+
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Contact>))]
+        public IActionResult GetAll() => Ok(repository.GetAll());
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created, Type = typeof(Contact))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Add([Required] [FromBody] Contact contact) {
+            if (contact.Id < 1 || string.IsNullOrWhiteSpace(contact.Firstname) || string.IsNullOrWhiteSpace(contact.Lastname) || string.IsNullOrWhiteSpace(contact.Email))
+                return BadRequest("Invalid input (e.g. required field missing or empty)");
+            repository.Add(contact);
+            return CreatedAtRoute(nameof(Add), contact);
+        }
+
+
+        [HttpDelete]
+        [Route("{personId}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int personId) {
+            if (personId < 1) return BadRequest("Invalid ID supplied");
+            try {
+                repository.Delete(personId);
+            }
+            catch (ArgumentException) {
+                return NotFound("Person not found");
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpGet("findByName")]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<Contact>))]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult FindByName([Required] [FromQuery] string nameFilter) {
+            IEnumerable<Contact> contacts;
+            try {
+                contacts = repository.FindByName(nameFilter);
+            }
+            catch (ArgumentException) {
+                return BadRequest("Invalid or missing name");
+            }
+
+            return Ok(contacts);
+        }
+    }
+}
---
+++ ContactList/ContactList/Services/ContactRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ContactList.Services {
+    public record Contact([Required] int Id, string Firstname, string Lastname, [Required] string Email);
+
+    public class ContactRepository {
+        private List<Contact> Contacts { get; } = new();
+
+        /// <summary>
+        /// Get all people in contact list
+        /// </summary>
+        public IEnumerable<Contact> GetAll() => Contacts;
+
+        /// <summary>
+        /// Adds a new person to the list of contacts
+        /// </summary>
+        /// <param name="contact">Person to add</param>
+        public void Add(Contact contact) => Contacts.Add(contact);
+
+        /// <summary>
+        /// Deletes a person from the list of contacts
+        /// </summary>
+        /// <param name="id">ID of person to delete</param>
+        public void Delete(int id) => Contacts.Remove(Contacts.FirstOrDefault(contact => contact.Id == id));
+
+        /// <summary>
+        /// Finds person in contact list by name
+        /// </summary>
+        public IEnumerable<Contact> FindByName(string nameFilter) {
+            List<Contact> contacts = Contacts.Where(contact => contact.Firstname.Contains(nameFilter) || contact.Lastname.Contains(nameFilter))
+                .ToList();
+            if (!contacts.Any()) throw new ArgumentException("Invalid or missing name");
+            return contacts;
+        }
+    }
+}
